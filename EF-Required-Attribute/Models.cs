5d2406e1c674a5bbc6e1e0282ed90f5a10a75b11
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-24 14:04:42 +0530

    Created

---
+++ EF-Required-Attribute/Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Required_Attribute
+{
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        [StringLength(50)]
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+    }
+
+
+
+
+}
